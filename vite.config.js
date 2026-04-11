6d9bf40a062d5a7e45ef58f5b5d8c29b1130ec37
lonlon343 <leonhartling96@googlemail.com>
2026-04-11 22:24:25 +0200

    Add Vite configuration for chunking and create Vercel rewrites file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Schneidet Firebase in eine eigene Datei
+          if (id.includes('node_modules/firebase')) {
+            return 'firebase';
+          }
+          // Schneidet React in eine eigene Datei
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
